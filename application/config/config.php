a0623d7b1caf06f5e203778fbec688f1305c9bc9
Mohamed Nisar <nizarp@gmail.com>
2017-08-09 15:42:46 +0530

    New URL

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://autoplanet.local/';
+$config['base_url']	= 'http://auto-planet.local/';
 
 /*
 |--------------------------------------------------------------------------
